4c49208aae4f5059e501e7bf226d3b015d175017
inu.shiba <shiba.to.choco@gmail.com>
2018-08-09 04:29:20 +0000

    DRY

--- app/controllers/tasks_controller.rb
+++ app/controllers/tasks_controller.rb
@@ -1,10 +1,12 @@
 class TasksController < ApplicationController
+  before_action :set_task, only: [:show, :edit, :update, :destroy]
+  
   def index
-    @tasks = Task.all
+    @task = Task.all
   end
 
   def show
-    @task = Task.find(params[:id])
+    set_task
   end
 
   def new
@@ -12,7 +14,7 @@ class TasksController < ApplicationController
   end
 
   def create
-    @task = Task.new(task_params)
+    @task = Task.new(message_params)
 
     if @task.save
       flash[:success] = 'Task が正常に投稿されました'
@@ -23,21 +25,13 @@ class TasksController < ApplicationController
     end
   end
 
-  private
-
-  # Strong Parameter
-  def task_params
-    params.require(:task).permit(:content)
-  end
-
   def edit
-    @task = Task.find(params[:id])
+    set_task
   end
 
   def update
-    @task = Task.find(params[:id])
-
-    if @task.update(message_params)
+    set_task
+    if @task.update(task_params)
       flash[:success] = 'Task は正常に更新されました'
       redirect_to @task
     else
@@ -47,10 +41,20 @@ class TasksController < ApplicationController
   end
 
   def destroy
-    @task = Task.find(params[:id])
+    set_task
     @task.destroy
 
     flash[:success] = 'Task は正常に削除されました'
-    redirect_to task_url
+    redirect_to tasks_url
+  end
+
+  private
+
+  def set_task
+    @task = Task.find(params[:id])
+  end
+
+  def task_params
+    params.require(:task).permit(:content)
   end
 end
